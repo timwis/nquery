4c1bc1f4107789a883e64d68aac376f2e4f01f51
timwis <tim@timwis.com>
2015-07-30 22:04:16 -0400

    Add soda tests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-exports.parse         = require('./lib/parser').parse;
+var qs = require('querystring');
+
 exports.tplParse      = require('./lib/parser').tplParse;
 
 exports.Table         = require('./lib/table');
@@ -12,3 +13,22 @@ exports.Extractor     = require('./lib/extractor');
 exports.AstHelper     = require('./lib/ast_helper');
 
 exports.AstReader     = require('./lib/ast_helper').Reader;
+
+exports.parse         = function(params) {
+  // If a string was passed, parse the querystring into an object
+  if(typeof params === 'string') {
+    params = qs.parse(params);
+  }
+
+  // Construct SQL string to be parsed
+  var sql = 'SELECT ' + (params.$select || '*');
+  if(params.$where) sql += ' WHERE ' + params.$where;
+  if(params.$group) sql += ' GROUP BY ' + params.$group;
+  if(params.$order) sql += ' ORDER BY ' + params.$order;
+  if(params.$limit) {
+    sql += ' LIMIT ' + params.$limit;
+    if(params.$offset) sql += ', ' + params.$offset;
+  }
+
+  return require('./lib/parser').parse(sql);
+};
---
+++ test/soda/select.js
@@ -0,0 +1,37 @@
+var should = require('should');
+var Parser = require('../../index');
+
+function inspect(obj) {
+  console.log(require('util').inspect(obj, false, 10, true));
+}
+
+describe('select test',function(){
+
+  it('multiple fields', function() {
+    var ast = Parser.parse('$select=foo, bar')
+    ast.columns.length.should.eql(2)
+  });
+
+  it('field alias', function() {
+    var ast = Parser.parse('$select=foo, bar as baz')
+    ast.columns[1].as.should.eql('baz')
+  });
+
+  it('functions', function() {
+    var ast = Parser.parse('$select=count(sheep) as total_sheep, date_trunc_ym(datetime) as year')
+    ast.columns[0].expr.type.should.eql('aggr_func')
+    ast.columns[0].expr.name.should.eql('COUNT')
+    ast.columns[0].as.should.eql('total_sheep')
+
+    ast.columns[1].expr.name.should.eql('date_trunc_ym')
+  });
+
+  it('operators', function() {
+    var ast = Parser.parse('$select=location, depth * 3.28 AS depth_feet')
+    //inspect(ast)
+    ast.columns[1].expr.operator.should.eql('*')
+    ast.columns[1].expr.left.column.should.eql('depth')
+    ast.columns[1].expr.right.value.should.eql(3.28)
+  });
+
+});
